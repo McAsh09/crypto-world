3a664a168708c5f92c58ca8f0f86892fbe310fa4
McAsh09 <mcash0907@gmail.com>
2022-02-25 08:52:47 +0530

    components added

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+
+import  { useGetCryptosQuery } from '../services/cryptoApi';
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
+
+const { Title } = Typography;
+const Homepage = () => {
+const {data, isFetching} = useGetCryptosQuery(10);
+
+  console.log(data);
+
+  if(isFetching) return 'Loading...' ;
+
+  const globalData = data?.data?.stats;
+  return (
+    <>
+      <Title level={2} className='heading'>
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalData.total}  /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={globalData.totalExchanges} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap:" value={millify(globalData.totalMarketCap)}/></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalData.total24hVolume)}/></Col>       
+        <Col span={12}><Statistic title="Total Markets" value={millify(globalData.totalMarkets)}/></Col>
+      </Row>
+      
+      <div className='home-heading-container'> 
+        <Title level={2} className='home-title'>Top 10 Cryptos in the World</Title>
+        <Title level={2} className='show-more'><Link to='/cryptocurrencies'>Show More</Link></Title>
+      </div>
+
+      <Cryptocurrencies simplified={true} />
+      
+      <div className='home-heading-container'> 
+        <Title level={2} className='home-title'>Latest Crypto News</Title>
+        <Title level={2} className='show-more'><Link to='/news'>Show More</Link></Title>
+      </div>
+
+      <News simplified/>
+    </>
+  );
+}
+
+export default Homepage
